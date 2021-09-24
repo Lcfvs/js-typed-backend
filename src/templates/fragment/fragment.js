02dea938dec3b0dd6226e2101e476b2b3d58929e
Lcfvs <lcf.vs@hotmail.com>
2021-09-24 08:02:00 +0200

    Improves the templates paths

--- src/templates/fragment/fragment.js
+++ src/templates/fragment/fragment.js
@@ -1,7 +1,7 @@
 import config from '@src/lib/config.js'
 import * as renderer from '@src/lib/renderer/renderer.js'
 import type, { fulfilled } from '@src/lib/type/type.js'
-import * as view from '../view/view.js'
+import * as view from '@src/templates/view/view.js'
 
 export const { fill, template } = await renderer.load(
   import.meta,
--- src/templates/layout/layout.js
+++ src/templates/layout/layout.js
@@ -2,7 +2,7 @@ import * as renderer from '@src/lib/renderer/renderer.js'
 import * as string from '@src/lib/type/string.js'
 import type, * as types from '@src/lib/type/type.js'
 import config from '@src/lib/config.js'
-import * as view from '../view/view.js'
+import * as view from '@src/templates/view/view.js'
 
 export const { fill, template } = await renderer.load(
   import.meta,
